2f5f07b28e9774aef41d935583264e0a1e9d7291
Benjamin Rühl <dev.ruehl@gmail.com>
2024-02-19 16:54:12 +0100

    feat: switch to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://benruehl.github.io',
-    base: '/astro-kruemelkiste',
+    site: 'https://steffis-kruemelkiste.de',
+    base: '',
     scopedStyleStrategy: 'class',
     vite: {
         css: {
