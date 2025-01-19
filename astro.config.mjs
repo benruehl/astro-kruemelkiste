f0272d1671fa76c14b216274f9204b69881772a5
Benjamin Rühl <dev.ruehl@gmail.com>
2025-01-19 12:29:23 +0100

    chore: migrate to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
         css: {
             preprocessorOptions: {
                 scss: {
-                    additionalData: '@import "./src/styles/main.scss";',
+                    additionalData: `@use "/src/styles/main" as *;`,
                 },
             },
         },
